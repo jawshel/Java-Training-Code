6d4931b66b9ae957e1434f4e73b3784da601054c
Pakingan, Marc Joeshel <mjoeshel@gmail.com>
2024-09-10 08:48:33 +0000

    Done adding output

---
+++ TaskSheet113/TaskSheet113.java
@@ -0,0 +1,32 @@
+package TaskSheet113;
+
+public class TaskSheet113 {
+  public static void main(String[] args) {
+
+    boolean Boo = true;
+    byte FirstBite = 0;
+    byte SecondBite = 1;
+    char D = 'd';
+    char Space = ' ';
+    short Shorty = 3;
+    int Int = 11;
+    float Float = 2.0F;
+
+    String outputString = "H"
+                    + Shorty
+                    + Int
+                    + FirstBite
+                    + " w"
+                    + FirstBite
+                    + "r"
+                    + SecondBite
+                    + D
+                    + Space
+                    + Float
+                    + Space
+                    + Boo;
+    System.out.println(outputString);
+
+  }
+    
+}
