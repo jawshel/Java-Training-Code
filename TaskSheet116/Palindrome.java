602a79c7192db9804941ae077b0bdcdc8c4666d8
Pakingan, Marc Joeshel <mjoeshel@gmail.com>
2024-09-10 09:11:31 +0000

    Done adding output

---
+++ TaskSheet116/Palindrome.java
@@ -0,0 +1,18 @@
+package TaskSheet116;
+
+import java.util.Scanner;
+
+ public class Palindrome {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter a word or characters: ");
+    String input = scanner.nextLine().replace(" ", "").toLowerCase();
+    String reversedInputString = new StringBuilder(input).reverse().toString();
+
+        if (input.equals(reversedInputString)) System.out.println("The input string is a palindrome");
+     else System.out.println("The input string is not a palindrome");
+
+    scanner.close();
+    }
+ }
